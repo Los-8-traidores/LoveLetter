2737573ff2bd1f06d4246d592755670be47dd88f
shoveli <matiasnicolasbeltramone@gmail.com>
2020-09-27 22:41:22 -0300

    Constructores, getPoint

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,52 @@
 package model;
 
 public class Player {
+	
+	protected int id;
+	private String name;
+	private int points;
+	private char state;
+	private Hand hand;
+	private int discards;
+	private boolean IsTurn;
+	
+	
+	
+	public Player(int id, String name, int points, char state, Hand hand, int discards, boolean isTurn) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.points = points;
+		this.state = state;
+		this.hand = hand;
+		this.discards = discards;
+		IsTurn = isTurn;
+	}
+
+
+
+	public Player() {
+		id = 0;
+		name = "NoName";
+		points = 0;
+		state = 'p';
+		Hand = new Hand();
+		IsTurn = false;
+	}
+	
+	
+	
+	public int getPoints() {
+		return points;
+	}
+	
+	public Player target() {
+		
+	}
+	
+	private Card chooseCard() {
+		
+	}
+	
 
 }
