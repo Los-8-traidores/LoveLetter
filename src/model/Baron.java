013e63dadfc55bc76c01db1edc894f118d56b8cb
Juan Sesan <jsesan00@gmail.com>
2020-09-30 20:34:37 -0300

    Baron/Countess/Priest/Prince
    Agregamos las clases Baron, Countess, Priest y Prince. Simplemente las creamos todavia estan lejos de ser usables para algo.

---
+++ src/model/Baron.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Baron extends Card{
+	public Baron (int strenght, String name, String effectDescription) {
+		super(strenght, name, effectDescription);
+	}
+
+	@Override
+	void effect() {
+
+	}
+}
---
+++ src/model/Countess.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Countess extends Card{
+	public Countess(int strenght, String name, String effectDescription) {
+		super(strenght, name, effectDescription);
+	}
+
+	@Override
+	void effect() {
+
+	}
+}
